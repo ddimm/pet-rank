f265e1b21172d4aa90a5768f0fcccc19da809071
William <wph2tu@virginia.edu>
2020-05-22 00:49:29 -0400

    added data querying to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
-import { Box } from "grommet";
+import { Box, Text } from "grommet";
+import { firebase } from "../utils/firebase";
+
+const db = firebase.firestore();
 
 export default function Homepage() {
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    db.collection("posts").onSnapshot((querySnapshot) => {
+      let postsTemp = [];
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, doc.data());
+        postsTemp.push(doc.data());
+      });
+      setPosts(postsTemp);
+    });
+  }, []);
+
   return (
     <Box>
-      <Post
+      {/* <Post
         title="This is a title"
         body="Lorem ipsum dolor sit amet,
               consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut
               labore et dolore magna aliqua."
-      />
+      /> */}
+      {!posts && <Text>Loading...</Text>}
+      {posts &&
+        posts.map((post) => {
+          return <Post title={post.title} body={post.body} />;
+        })}
+      {posts && posts.length === 0 && <Text>No posts :(</Text>}
     </Box>
   );
 }
